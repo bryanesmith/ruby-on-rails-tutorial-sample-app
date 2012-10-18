eba26112c12d4ade1ae4543af51e0837ecc90e69
Bryan Smith <besmit@umich.edu>
2012-10-18 14:33:20 -0400

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,11 +20,19 @@ class User < ActiveRecord::Base
   # case-insensitive uniqueness enforced by the index works)
   before_save { |user| user.email = email.downcase }
 
+  # Generate token
+  before_save :create_remember_token
+
   VALID_EMAIL_REGEX = /^[\w+\-.]+@[a-z\d\-.]+\.[a-z]+$/i
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: {minimum: 6}
   validates :password_confirmation, presence: true
+
+  private
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 
 end
